a8fa8513a70e04896de769cc52ded5757338a231
Retse_dev <stergaldeano@gmail.com>
2024-01-27 15:06:19 +0100

    player-jump-animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 const SPEED = 190.0
 const JUMP_VELOCITY = -510.0
 
+var previousPosition = Vector2.ZERO
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,12 +15,15 @@ func _ready():
 	pass
 	
 func updateAnimation():
-	if velocity.x ==0: animations.play("idle")
-	if velocity.x != 0: animations.play("run")
-	if Input.is_key_pressed(KEY_SPACE): 
+	if position.y < previousPosition.y: 
 		animations.play("jump")
-	
-	
+	elif position.y > previousPosition.y: 
+		animations.play("fall_normal")
+		print_debug("falll")
+	elif velocity.x != 0:
+		animations.play("run")
+	else:
+		animations.play("idle")
 	
 	if Input.is_action_just_pressed("ui_left"):
 		playerSprite.flip_h=true
@@ -34,6 +39,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	previousPosition = position
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
