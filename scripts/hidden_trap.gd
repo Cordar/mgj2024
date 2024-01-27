359ca78f64be7e7e94062c103cedc740bd42532a
Cmomnogmgg <diego_mzmn@hotmail.com>
2024-01-27 10:42:08 +0100

    Add phantom camera addon

---
+++ scripts/hidden_trap.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func _ready():
+	#var area2d = $"Area2d"
+	#area2d.connect("area_entered",self,"_hit_building")
+	#area2d.visible = false
+
+func _hit_building(area):
+	#area.visible = true
