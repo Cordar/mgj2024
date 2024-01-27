5c3c0757e3c20d124f6b16037f72de093504a0e7
Cmomnogmgg <diego_mzmn@hotmail.com>
2024-01-27 10:43:18 +0100

    Revert "Add phantom camera addon"
    This reverts commit 359ca78f64be7e7e94062c103cedc740bd42532a.

--- scripts/hidden_trap.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready():
-	#var area2d = $"Area2d"
-	#area2d.connect("area_entered",self,"_hit_building")
-	#area2d.visible = false
-
-func _hit_building(area):
-	#area.visible = true
