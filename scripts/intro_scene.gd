d5c88e40b496bf0ebd81b660ecfa882c0ce7d728
Retse_dev <stergaldeano@gmail.com>
2024-01-27 20:49:19 +0100

    intro working

--- scripts/intro_scene.gd
+++ scripts/intro_scene.gd
@@ -1,6 +1,8 @@
 extends Control
 @onready var textLabel = $RichTextLabel
 
+const TEXT_SPEED = 0.03
+
 func _on_animation_player_animation_finished(anim_name:StringName):
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
@@ -8,7 +10,7 @@ func _on_animation_player_animation_finished(anim_name:StringName):
 
 func delayText(text: String):
 	for i in range(0,text.length()):
-		await get_tree().create_timer(0.1).timeout
+		await get_tree().create_timer(TEXT_SPEED).timeout
 		textLabel.text += text[i]
 
 func cleanText():
