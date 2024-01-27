c9d3b45146619f11eb2cf0f60d00aff34ee5a12a
enric <enricmir1993@gmail.com>
2024-01-27 16:40:29 +0100

    intro scene before playing the game

--- scripts/intro_scene.gd
+++ scripts/intro_scene.gd
@@ -1,11 +1,5 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_player_animation_finished(anim_name:StringName):
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
