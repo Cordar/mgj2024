4815c6f752a02ff686a7471479d211d9eb1f1b14
enric <enricmir1993@gmail.com>
2024-01-27 11:57:44 +0100

    added album and save memes unlocked

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,9 +2,14 @@ extends Control
 
 
 func _on_start_button_pressed():
-	Globals.unlock_meme(Globals.Meme.Monkey)
+	Globals.unlock_meme(Globals.Meme.InfernalGirl)
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_album_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/UI/album.tscn")
+
+
+func _on_reset_save_data_pressed():
+	Globals.reset_game()
+	
