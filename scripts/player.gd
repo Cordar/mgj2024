5097733879031cd0539ffd5b40503b4a981dab9d
enric <enricmir1993@gmail.com>
2024-01-27 16:14:54 +0100

    intro scene things

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,9 +25,9 @@ func updateAnimation():
 	else:
 		animations.play("idle")
 	
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_just_pressed("move_left"):
 		playerSprite.flip_h=true
-	elif Input.is_action_just_pressed("ui_right"):
+	elif Input.is_action_just_pressed("move_right"):
 		playerSprite.flip_h=false
 
 func _physics_process(delta):
@@ -37,14 +37,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animations.play("jump")
 	previousPosition = position
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
